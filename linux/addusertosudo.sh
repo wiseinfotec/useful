5f99b06fc2a49b6f963b675706f1e43e7ecf134a
qdrddr <qdrddr@gmail.com>
2023-05-09 12:27:09 -0500

    Create addusertosudo.sh

---
+++ linux/addusertosudo.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+u=$1
+
+cd /etc/sudoers.d
+t=${u}" ALL=(ALL) NOPASSWD:ALL"
+
+echo ${t} > ${u}
+chmod 0440 ${u}
+
+echo ${t}
+ls -la
+
+adduser ${u}
+usermod -aG sudo ${u}
+
+mkdir -p /home/${u}/.ssh
+cd /home/${u}/.ssh
+touch authorized_keys
+chown -R ${u}:${u} /home/${u}/.ssh
+chmod 0755 -R /home/${u}/.ssh
+
+visudo -c
