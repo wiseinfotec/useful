2ff084499d005651ee8b9c1f5877be27c8b14660
qdrddr <qdrddr@gmail.com>
2023-05-09 13:20:43 -0500

    Update addusertosudo.sh

--- linux/addusertosudo.sh
+++ linux/addusertosudo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 u=$1
-s=$2
+$2
 
 cd /etc/sudoers.d
 t=${u}" ALL=(ALL) NOPASSWD:ALL"
@@ -17,7 +17,8 @@ usermod -aG sudo ${u}
 
 mkdir -p /home/${u}/.ssh
 cd /home/${u}/.ssh
-touch authorized_keys
+#touch authorized_keys
+cat $2 >> authorized_keys
 chown -R ${u}:${u} /home/${u}/.ssh
 chmod 0755 -R /home/${u}/.ssh
 
